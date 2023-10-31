32096a971c986dc66c1dbcece6355a74a78d4fea
jessika <jessikafujimura@gmail.com>
2023-10-31 14:01:40 -0300

    "add module in iml and fix import"

--- Exception Handling/Leitura e Processamento de arquivo/src/Main.java
+++ Exception Handling/Leitura e Processamento de arquivo/src/Main.java
@@ -1,8 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 public class Main {
     
     public static void main(String[] args){
         File file = new File("Color.txt");
-        String pathAbs = file.getAbsolutePath();
+        String pathAbs = System.getProperty("user.dir").concat("/" +file.getPath());
         
         List<String> data = readFileReader(pathAbs);
         
